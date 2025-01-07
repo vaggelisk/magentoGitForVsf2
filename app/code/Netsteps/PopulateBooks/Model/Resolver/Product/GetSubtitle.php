e21da4fb62853ea47255a82616f4473eb9499bb8
vaggelisk <you@example.com>
2025-01-07 12:15:11 +0200

    feat-add-customattribute-at-graphql

--- app/code/Netsteps/PopulateBooks/Model/Resolver/Product/GetSubtitle.php
+++ app/code/Netsteps/PopulateBooks/Model/Resolver/Product/GetSubtitle.php
@@ -1,14 +1,11 @@
 <?php
+
 namespace Netsteps\PopulateBooks\Model\Resolver\Product;
 
 use Magento\Catalog\Model\Product;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-
-use Psr\Log\LoggerInterface;
-use Zend_Log;
-use Zend_Log_Writer_Stream;
 
 class GetSubtitle implements ResolverInterface
 {
@@ -30,18 +27,6 @@ class GetSubtitle implements ResolverInterface
         $product = $value['model'];
         $productdata = $this->productdata->load($product->getId());
 
-//        $objectManager = \Magento\Framework\App\ObjectManager::getInstance(); // instance of object manager
-//        $product = $objectManager->create('\Magento\Catalog\Model\Product');
-//        return $product->getName();
-        $result = $productdata->getCustomAttribute('subtitle')->getValue();
-
-        $writer = new Zend_Log_Writer_Stream(BP . '/var/log/system.log');
-        $logger = new Zend_Log();
-        $logger->addWriter($writer);
-        $logger->log( print_r( gettype($result), 1),1);
-        $logger->log( print_r( $result, 1),1);
-
-
-        return $result;
+        return $productdata->getCustomAttribute('subtitle')->getValue();
     }
 }
